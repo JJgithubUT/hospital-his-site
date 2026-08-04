f04c3cbb1e03e238c042e74ada0b256310405351
Juan Javier <juanjcbreton@gmail.com>
2026-08-03 19:09:27 -0600

    Commit for actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  base: '/hospital-his-site/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    // Si el comando es 'serve' (npm run dev), usa '/'
+    // Si el comando es 'build' (npm run build), usa '/hospital-his-site/'
+    base: command === 'serve' ? '/' : '/hospital-his-site/',
+  }
 })
